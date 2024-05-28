556013e04285180a15f6d37e26efdeab56a31b44
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-28 22:43:20 +0530

    koko

--- main.tf
+++ main.tf
@@ -5,3 +5,62 @@ resource "aws_docdb_subnet_group" "main" {
   tags = var.tags
 }
 
+
+
+resource "aws_security_group" "main" {
+  name        = "${local.name_prefix}-sg"
+  description = "${local.name_prefix}-sg"
+  vpc_id      = var.vpc_id
+  tags = var.tags
+}
+
+
+
+resource "aws_security_group_rule" "main" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = aws_security_group.main.id
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "main" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+
+
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${local.name_prefix}-cluster"
+  engine                  = "docdb"
+  master_username         = data.aws_ssm_parameter.master_username.value
+  master_password         = data.aws_ssm_parameter.master_password.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
+  db_subnet_group_name = aws_docdb_subnet_group.main.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  tags = var.tags
+  engine_version = var.engine_version
+}
+
+
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = var.family
+  name        = "${local.name_prefix}-pg"
+  description ="${local.name_prefix}-pg"
+  tags = var.tags
+}
+
+resource "aws_docdb_cluster_instance" "main" {
+  count              = var.instance_count
+  identifier         = "${local.name_prefix}-clusterinstance-${count.index}"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,13 @@
 variable "tags" {}
 variable "env" {}
 variable "subnet_ids" {}
+
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "engine_version" {}
+variable "family" {}
+variable "instance_count" {}
+variable "instance_class" {}
